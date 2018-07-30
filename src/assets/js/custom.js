96aa424af269ceedebd29d4a1b090f137ad3dd45
Ricardo Gallardo <rgallardouk@gmail.com>
2018-07-30 11:24:38 +0100

    style: reordered class definitions

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,34 +1,3 @@
-// Enemies our player must avoid
-class Enemy{
-    constructor(x = 0, y = 0) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'assets/img/enemy-bug.png';
-
-    // X Y coordinates of the the enemy sprite
-    this.x = x;
-    this.y = y;
-    this.speed = 20;
-    };
-
-    // Update the enemy's position, required method for game
-    // Parameter: dt, a time delta between ticks
-    update(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    //this.x = this.x < 500 ? this.x + (this.speed * dt) : 0;
-    };
-
-    // Draw the enemy on the screen, required method for game
-    render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    };
-};
-
 /**
  *
  *
@@ -60,6 +29,39 @@ class Map {
         this.enemyMaxY = enemyMaxY;
     }
 }
+
+// Enemies our player must avoid
+class Enemy{
+    constructor(x = 0, y = 0) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'assets/img/enemy-bug.png';
+
+    // X Y coordinates of the the enemy sprite
+    this.x = x;
+    this.y = y;
+    this.speed = 20;
+    };
+
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    //this.x = this.x < 500 ? this.x + (this.speed * dt) : 0;
+    };
+
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
+};
+
+
 
 // Now write your own player class
 // This class requires an update(), render() and
